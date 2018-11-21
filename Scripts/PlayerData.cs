d44131b1558803e1a1c40085e137e97813a89057
Betty Gandelman <43202572+BetGand@users.noreply.github.com>
2018-11-21 14:06:14 +0200

    Add files via upload

---
+++ Scripts/PlayerData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[Serializable]
+public class PlayerData  {
+    public int health;
+    public int experiance;
+	// Use this for initialization
+	public PlayerData () {
+        health = 100;
+        experiance = 0;
+    }
+
+}
